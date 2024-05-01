fc065a87e0fd108a00ea520f9681eff4e9a6868d
Milind Mishra <milind.mishra4@gmail.com>
2024-05-02 02:49:18 +0530

    📦 NEW: update theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/footer";
 import Nav from "@/components/nav";
+import Footer from "@/components/footer";
 
 export default async function Home() {
   // Simulate a delay
