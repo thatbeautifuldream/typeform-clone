55d360bbc953a76b8a8fea2cf77e58adcecde5b8
Milind Mishra <milind.mishra4@gmail.com>
2024-05-06 11:55:34 +0530

    🐛 FIX: remove unnessasary deps

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
-import { useTheme } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const { theme } = useTheme();
-  const progressColor = theme === "dark" ? "#3B81F6" : "#2463EB";
   return (
     <>
       <ThemeProvider
@@ -17,12 +13,6 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         disableTransitionOnChange
       >
         {children}
-        <ProgressBar
-          height="4px"
-          color={progressColor}
-          options={{ showSpinner: false }}
-          shallowRouting
-        />
         <Toaster position="top-center" richColors />
       </ThemeProvider>
     </>
